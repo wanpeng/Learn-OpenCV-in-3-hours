ef9dbee0e53e0938930a19c5b9d69ecfbc704edc
wanp <peng.wan@wo.cn>
2021-03-27 21:53:43 +0800

    ch2 comments

--- chapter2.py
+++ chapter2.py
@@ -2,12 +2,21 @@ import cv2
 import numpy as np
 
 img = cv2.imread("Resources/lena.png")
+
+# np.ones 值 1 矩阵
+# uint8   0 ~ 255
 kernel = np.ones((5,5),np.uint8)
 
 imgGray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 imgBlur = cv2.GaussianBlur(imgGray,(7,7),0)
+
+# 边缘
 imgCanny = cv2.Canny(img,150,200)
+
+# 膨胀
 imgDialation = cv2.dilate(imgCanny,kernel,iterations=1)
+
+# 腐蚀
 imgEroded = cv2.erode(imgDialation,kernel,iterations=1)
 
 cv2.imshow("Gray Image",imgGray)
