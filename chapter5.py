76a88310208f57f538815612c541b0874a7873af
wanp <peng.wan@wo.cn>
2021-03-28 10:59:45 +0800

    ch5 comments PerspectiveTransform

--- chapter5.py
+++ chapter5.py
@@ -3,10 +3,20 @@ import numpy as np
 
 img = cv2.imread("Resources/cards.jpg")
 
+# 卡片大小
 width,height = 250,350
+
 pts1 = np.float32([[111,219],[287,188],[154,482],[352,440]])
 pts2 = np.float32([[0,0],[width,0],[0,height],[width,height]])
+
+# 图片 视角变换 斜图 放平
+
+# 获得投射变化后的H矩阵
+# https://docs.opencv.org/2.4/modules/imgproc/doc/geometric_transformations.html#getperspectivetransform
 matrix = cv2.getPerspectiveTransform(pts1,pts2)
+
+# 获得变化后的图像
+# https://docs.opencv.org/2.4/modules/imgproc/doc/geometric_transformations.html#warpperspective
 imgOutput = cv2.warpPerspective(img,matrix,(width,height))
 
 
